2af9d6a8cf1249688bac518bad52b10df1e77e8b
Marat Vildanov <mtmy@yandex.ru>
2025-01-28 18:45:47 +0500

    1

--- 3 Объекты и классы/3.2.2.1 Наследование практика.py
+++ 3 Объекты и классы/3.2.2.1 Наследование практика.py
@@ -1,49 +1,56 @@
-from datetime import datetime
-
 class Employee:
     vacation_days = 28
 
+    #def __generate_employee_id(self):
+
+        #return hash(self.first_name + self.second_name + self.gender)
+    
+ 
     def __init__(self, first_name, second_name, gender):
         self.first_name = first_name
         self.second_name = second_name
         self.gender = gender
         self.remaining_vacation_days = Employee.vacation_days
+        self._employee_id = self.__generate_employee_id()
+        
+    def __generate_employee_id(self):
 
+        return hash(self.first_name + self.second_name + self.gender)
+
+    
+
+ 
     def consume_vacation(self, days):
         self.remaining_vacation_days -= days
 
     def get_vacation_details(self):
         return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
 
-# Расширьте класс Employee, создав классы FullTimeEmployee и PartTimeEmployee.
+
 class FullTimeEmployee(Employee):
-    
-    def __init__(self, first_name, second_name, gender):
-        self.first_name = first_name
-        self.second_name = second_name
-        self.gender = gender
-        self.remaining_vacation_days = Employee.vacation_days
 
+    def __init__(self, first_name, second_name, gender, salary):
+        super().__init__(first_name, second_name, gender)
+        self.__salary = salary
 
-    def get_unpaid_vacation(self, date, unpaid_vocation_duration):
-        self.date = datetime.strptime(date, '%Y-%m-%d')
-        self.unpaid_vocation_duration = unpaid_vocation_duration
-        self.formated_date = datetime.strftime(self.date, '%Y-%m-%d')
-        return f'Начало неоплачиваемого отпуска: {self.formated_date}, продолжительность: {self.unpaid_vocation_duration} дней.'
+    def get_unpaid_vacation(self, start_date, days):
+        return f'Начало неоплачиваемого отпуска: {start_date}, продолжительность: {days} дней.'
 
-
+    def __get_vacation_salary(self):
+        return self.__salary * 0.8
 
 class PartTimeEmployee(Employee):
     vacation_days = 14
 
     def __init__(self, first_name, second_name, gender):
-        self.first_name = first_name
-        self.second_name = second_name
-        self.gender = gender
+        super().__init__(first_name, second_name, gender)
         self.remaining_vacation_days = PartTimeEmployee.vacation_days
-        
+
+
 # Пример использования:
-full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
+full_time_employee = FullTimeEmployee('Иван', 'Иванов', 'м', 50000)
 print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
-part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
+
+part_time_employee = PartTimeEmployee('Анна', 'Петрова', 'ж')
+part_time_employee.consume_vacation(5)
 print(part_time_employee.get_vacation_details())
