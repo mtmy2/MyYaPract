2db280b2fa1de4014387ed966da497e0531a739f
Marat Vildanov <mtmy@yandex.ru>
2025-04-24 06:47:59 +0500

    1

--- 6 Продвинутый SQL/6.6.4 Project GastroHub.sql
+++ 6 Продвинутый SQL/6.6.4 Project GastroHub.sql
@@ -153,3 +153,15 @@ SELECT
 FROM grouped_per_year;
 
 
+/*Задание 3
+Найдите топ-3 заведения, где чаще всего менялся менеджер за весь период.*/
+
+SELECT 
+	r.name,
+	count(*)
+FROM cafe.restaurant_manager_work_dates
+LEFT JOIN cafe.restaurants AS r 
+USING(restaurant_uuid)
+GROUP BY r.name
+ORDER BY count(*) DESC
+LIMIT 3
